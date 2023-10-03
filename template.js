33aae43ea0a48315d953e538d5dbb79ecaa22a50
lucasrigonato <lucas.rigonatto@gmail.com>
2023-10-03 11:22:20 -0300

    submit

--- template.js
+++ template.js
@@ -1,12 +1,22 @@
 // template.js
 const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Navbar</a>
-    </nav>
-  );
-}
-
+    return (
+      <nav className="navbar navbar-expand navbar-light bg-light">
+        <a className="navbar-brand" href="index.html">Navbar</a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="index.html">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="press.html">Press</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+  
 const Sidebar = () => {
   return (
     <div className="d-none d-md-block col-md-3">
